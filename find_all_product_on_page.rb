604f8c2fe3f01e3adc6b62a9a870fea26c5a5e0b
petro12330 <52936784+petro12330@users.noreply.github.com>
2020-12-04 21:47:11 +0300

    Update find_all_product_on_page.rb

--- find_all_product_on_page.rb
+++ find_all_product_on_page.rb
@@ -1,47 +1,69 @@
 require 'curb'
+require 'curb'
 require 'nokogiri'
 require 'csv'
-url_page='https://www.petsonic.com/comida-lata-para-perros/'
-file_name=gets.chomp()
-status=true#станет false, когда не найдет на странице текст "следующая стр"
-number_page=2
-page=''
-full_list=[]
-while status # понимаю насколько это медленно и глупо, но пока другой алгоритм не подобрал
-  url=url_page+page
-  c = Curl::Easy.perform(url)
-  doc = Nokogiri::HTML(c.body_str)
-  kol_vo=doc.xpath("//button[@class='loadMore next button lnk_view btn btn-default']/span/text()").count
-  if kol_vo==1
-    page='?p='+number_page.to_s
-    number_page=number_page+1
-    links=doc.xpath("//div[@class='product-desc display_sd']/a/@href")
-    full_list+=links
-  else
-    status=false
+
+class Product
+
+  def initialize (url)
+    @html = Nokogiri::HTML ((Curl::Easy.perform(url)).body_str)
   end
-end# ооооооочень долгая конструкция, оптимизирую ее первым делом(если этот комент висит, значит не опитимизировал((()
-puts 'Нашел список всех продуктов. Я насчитал '+ full_list.count.to_s
 
-CSV.open(file_name+".csv","w") do |wr|
-  wr << ["Name", "price", "image"]
-end
-
-iskl=[]
-for i in full_list
-  url=i
-  c = Curl::Easy.perform(url)
-  doc = Nokogiri::HTML(c.body_str)
-  puts 'Записываю данные с '+i
-  img=doc.xpath("//span[@id='view_full_size']/img/@src")
-  name=doc.xpath("//h1/text()")
-  size=doc.xpath("//label/span[@class='radio_label']/text()")
-  price=doc.xpath("//label/span[@class='price_comb']/text()")
-
-  CSV.open(file_name+".csv","a") do |a|
-    for i in 0..(size.count-1) do
-      a << [(name.to_s+' - '+size[i].to_s), price[i],img]
+  def get_info (file_name)
+    @file_name = file_name
+    @img = @html.xpath("//span[@id='view_full_size']/img/@src")
+    @name = @html.xpath("//h1/text()")
+    @size = @html.xpath("//label/span[@class='radio_label']/text()")
+    @price = @html.xpath("//label/span[@class='price_comb']/text()")
+    CSV.open(@file_name+".csv","a") do |a|
+      for i in 0..(@size.count-1) do
+        a << [(@name.to_s+' - '+@size[i].to_s), @price[i],@img]
+      end
     end
   end
 end
-puts "Файл "+ file_name+'записан.'
+
+class All_products
+
+  def initialize (url, file_name)
+    @url = url
+
+    @file_name= file_name
+
+  end
+
+  def products_list
+    puts 'Начал'
+    @number_page = 2
+    @page = ''
+    @full_list= []
+    @n= 1
+    CSV.open(@file_name+".csv","w") do |wr|
+      wr << ["Name", "price", "image"]
+    end
+    @threads = []
+    while Curl::Easy.perform(@url + @page).status.to_s == '200'
+      @doc = Nokogiri::HTML ((Curl::Easy.perform(@url + @page)).body_str)
+
+      @page = '?p=' + @number_page.to_s
+      @number_page = @number_page + 1
+      @links = @doc.xpath("//div[@class='product-desc display_sd']/a/@href")
+      @full_list += @links # list with all url
+      for @link in @links
+        Product.new(@link).get_info (@file_name)
+        puts 'Выполняю ', @link
+      end
+    end
+  end
+end
+
+url_page = 'https://www.petsonic.com/comida-lata-para-perros/' #exemple
+
+puts "Enter URL"
+url_page = gets.chomp
+
+puts "Enter File name"
+file_name = gets.chomp
+
+prod = All_products.new(url_page, file_name)
+prod.products_list
